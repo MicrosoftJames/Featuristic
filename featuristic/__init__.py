c138547388feb410ebf98caa911b5ed0380eb783
James <vaughanjames@microsoft.com>
2025-03-05 11:26:08 +0000

    Add __init__.py to expose featuristic module and features

---
+++ featuristic/__init__.py
@@ -0,0 +1,5 @@
+from .feature import Feature, FeatureDefinition, PromptFeatureDefinition, PromptFeatureDefinitionGroup
+from .featuristic import Featuristic
+
+__all__ = ["Featuristic", "Feature", "FeatureDefinition",
+           "PromptFeatureDefinition", "PromptFeatureDefinitionGroup"]
