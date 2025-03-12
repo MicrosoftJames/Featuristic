caabf1b93d78f0cf20e1806d6cded64679e33ac0
James <vaughanjames@microsoft.com>
2025-03-12 15:03:18 +0000

    Update __all__ in __init__.py to include FeaturisticClassifier and related features

--- featuristic/__init__.py
+++ featuristic/__init__.py
@@ -1,3 +1,8 @@
-from featuristic import features
+from featuristic.core import FeaturisticClassifier
+from featuristic.features.feature import Feature, PromptFeatureConfiguration, \
+    PromptFeatureDefinition, FeatureDefinition
+from featuristic.features.feature_extractor import extract_features
+from featuristic.classification.naive_bayes_classifier import Distribution
 
-__all__ = ["features"]
+__all__ = ["FeaturisticClassifier", "Feature", "PromptFeatureConfiguration",
+           "PromptFeatureDefinition", "FeatureDefinition", "extract_features", "Distribution"]
