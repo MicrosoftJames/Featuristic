b80ed6f1217a9c94a9c2c80493a26de59cd8d0be
James <vaughanjames@microsoft.com>
2025-03-12 15:09:06 +0000

    Rename feature_extractor.py to extract.py

--- featuristic/__init__.py
+++ featuristic/__init__.py
@@ -1,7 +1,7 @@
 from featuristic.core import FeaturisticClassifier
 from featuristic.features.feature import Feature, PromptFeatureConfiguration, \
     PromptFeatureDefinition, FeatureDefinition
-from featuristic.features.feature_extractor import extract_features
+from featuristic.features.extract import extract_features
 from featuristic.classification.naive_bayes_classifier import Distribution
 
 __all__ = ["FeaturisticClassifier", "Feature", "PromptFeatureConfiguration",
