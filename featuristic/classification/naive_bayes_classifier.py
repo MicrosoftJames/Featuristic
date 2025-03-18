a2adcd437a0dcd3b9716d4edf74fbcb485d487df
James <vaughanjames@microsoft.com>
2025-03-18 09:42:24 +0000

    Add docstrings to Naive Bayes classifier classes and methods for better documentation

--- featuristic/classification/naive_bayes_classifier.py
+++ featuristic/classification/naive_bayes_classifier.py
@@ -8,6 +8,7 @@ from featuristic.classification.mixed_type_naive_bayes import predict, predict_p
 
 
 class Distribution(Enum):
+    """An enum to represent the different types of Naive Bayes classifiers."""
     MULTINOMIAL = 1
     BERNOULLI = 2
     GAUSSIAN = 3
@@ -15,23 +16,43 @@ class Distribution(Enum):
 
 @dataclass
 class _NBClassifier():
+    """A dataclass to hold the classifier settings for each column.
+
+    Attributes:
+        nb_classifier(_BaseNB): The classifier to use.
+        data_slice(slice): The slice of the data to used for this classifier.
+        expand_multinomial_col(bool): Whether to expand the multinomial column into two columns.
+    """
     nb_classifier: _BaseNB
     data_slice: slice
     expand_multinomial_col: bool
 
 
 class MixedTypeNaiveBayesClassifier():
+    """A class that implements a Naive Bayes classifier that can handle mixed types of data.
+    This class is a wrapper around the sklearn Naive Bayes classifiers and allows for
+    different types of classifiers to be used for different columns of the data.
+    """
+
     def __init__(self):
         self._classifier_settings: List[_NBClassifier] = []
 
-    def add_classifier(self, classifier: Distribution, data_slice: slice, classifier_args: dict = {}, expand_multinomial_col=False):
-        if classifier == Distribution.MULTINOMIAL:
+    def add_classifier(self, distribution: Distribution, data_slice: slice, classifier_args: dict = {}, expand_multinomial_col=False):
+        """Adds a classifier to the list of classifiers.
+
+        Args:
+            distribution (Distribution): The type of distribution to use for the classifier.
+            data_slice (slice): The slice of the data columns to used for this classifier.
+            classifier_args (dict): The arguments to pass to the corresponding sklearn classifier.
+            expand_multinomial_col (bool): Whether to expand the multinomial column into two columns.
+        """
+        if distribution == Distribution.MULTINOMIAL:
             nb_classifier = MultinomialNB(**classifier_args)
 
-        elif classifier == Distribution.BERNOULLI:
+        elif distribution == Distribution.BERNOULLI:
             nb_classifier = BernoulliNB(**classifier_args)
 
-        elif classifier == Distribution.GAUSSIAN:
+        elif distribution == Distribution.GAUSSIAN:
             nb_classifier = GaussianNB(**classifier_args)
 
         self._classifier_settings.append(
@@ -58,6 +79,11 @@ class MixedTypeNaiveBayesClassifier():
                 "The slices must start from 0 and end at the last column index")
 
     def fit(self, X, Y):
+        """Fits the Naive Bayes classifiers to the data.
+        Args:
+            X (np.ndarray): The feature matrix.
+            Y (np.ndarray): The target vector.
+        """
         self._validate_slices(X)
         for classifer_settings in self._classifier_settings:
             X_slice = X[:, classifer_settings.data_slice]
@@ -66,13 +92,31 @@ class MixedTypeNaiveBayesClassifier():
             classifer_settings.nb_classifier.fit(X_slice, Y)
 
     def predict(self, X) -> np.ndarray:
+        """Predicts the class labels for the data.
+        Args:
+            X (np.ndarray): The feature matrix.
+        Returns:
+            np.ndarray: The predicted class labels.
+        """
         return predict([cs.nb_classifier for cs in self._classifier_settings],
                        [X[:, cs.data_slice] if not cs.expand_multinomial_col else self._expand_proportions(X[:, cs.data_slice]) for cs in self._classifier_settings])
 
     def predict_proba(self, X) -> np.ndarray:
+        """Predicts the class probabilities for the data.
+        Args:
+            X (np.ndarray): The feature matrix.
+        Returns:
+            np.ndarray: The predicted class probabilities.
+        """
         return predict_proba([cs.nb_classifier for cs in self._classifier_settings],
                              [X[:, cs.data_slice] if not cs.expand_multinomial_col else self._expand_proportions(X[:, cs.data_slice]) for cs in self._classifier_settings])
 
     def predict_log_proba(self, X) -> np.ndarray:
+        """Predicts the log-probabilities of the class labels for the data.
+        Args:
+            X (np.ndarray): The feature matrix.
+        Returns:
+            np.ndarray: The predicted log-probabilities of the class labels.
+        """
         return predict_log_proba([cs.nb_classifier for cs in self._classifier_settings],
                                  [X[:, cs.data_slice] if not cs.expand_multinomial_col else self._expand_proportions(X[:, cs.data_slice]) for cs in self._classifier_settings])
