e7a4de05adfbc4591093c6655528e50d54265bc6
James <vaughanjames@microsoft.com>
2025-03-19 16:41:14 +0000

    Refactor _extract_features_with_llm function to use litellm for feature extraction

--- featuristic/features/llm.py
+++ featuristic/features/llm.py
@@ -1,14 +1,24 @@
-from langchain_core.messages import HumanMessage, SystemMessage
-from langchain_openai import AzureChatOpenAI
+from litellm import acompletion, supports_response_schema
 from pydantic import BaseModel
 
 
 async def _extract_features_with_llm(text, schema: BaseModel, system_prompt: str, aoai_api_key: str, aoai_api_endpoint: str, gpt4o_deployment: str = "gpt-4o"):
     """Extract features using the Azure OpenAI LLM."""
-    llm = AzureChatOpenAI(
-        azure_deployment=gpt4o_deployment, api_key=aoai_api_key, azure_endpoint=aoai_api_endpoint, api_version="2024-08-01-preview", temperature=0)
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": text}
+    ]
 
-    llm = llm.with_structured_output(schema)
+    if not supports_response_schema(model=gpt4o_deployment):
+        raise ValueError(
+            "The provided model does not support json_schema response format.")
 
-    return await llm.ainvoke(
-        [SystemMessage(system_prompt), HumanMessage(text)])
+    resp = await acompletion(model=gpt4o_deployment,
+                             messages=messages,
+                             response_format=schema,
+                             api_key=aoai_api_key,
+                             base_url=aoai_api_endpoint,
+                             api_version="2024-08-01-preview",
+                             temperature=0)
+
+    return schema.model_validate_json(resp.choices[0].message.content)
